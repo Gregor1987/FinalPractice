29bd4b3dfe18e51814f293842767b7aad9de9c35
Aleksandr <Gregor1987@mail.ru>
2022-09-21 19:27:28 +0500

    added method for array printing and some changes on program output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите длинну массива:");
+﻿Console.Write("Введите длинну массива: ");
 int arrayLength = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[arrayLength];
 for (int i = 0; i < arrayLength; i++)
@@ -11,19 +11,32 @@ for (int i = 0; i < arrayLength; i++)
 {
     if (array[i].Length <= 3) newArrayLength++;
 }
+void ArrayPrint(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+
+}
 if (newArrayLength == 0) Console.WriteLine("В введенном массиве нет элементов, длина которых меньше либо равна 3");
 else
 {
+    Console.WriteLine("Первоначальный массив:");
+    ArrayPrint(array);
     string[] newArray = new string[newArrayLength];
     int j = 0;
+    Console.WriteLine();
     Console.WriteLine("Элементы, длина которых меньше либо равна 3:");
     for (int i = 0; i < arrayLength; i++)
     {
         if (array[i].Length <= 3) 
         {
             newArray[j] = array[i];
-            Console.Write($"{newArray[j]} ");
             j++;
         }
     }
+    ArrayPrint(newArray);
 }
